e83ee0b90711ff0e03397ea5883cf9a6cd377cb1
liliu-z <li.liu@zilliz.com>
2026-05-21 21:49:04 +0800

    Update stashbase to 0.2.0

--- Casks/stashbase.rb
+++ Casks/stashbase.rb
@@ -1,8 +1,8 @@
 cask "stashbase" do
-  version "0.1.0"
-  sha256 "956c01195d76a5f5fd9f8fb0b0c15e144e424335444b9adbf03560178ecae240"
+  version "0.2.0"
+  sha256 "d545ecd5733718fbeb66185a9977249070e744a44b78ba134bf1d8662b107ad7"
 
-  url "https://github.com/liliu-z/stashbase/releases/download/v0.1.0/StashBase-0.1.0-mac-arm64.dmg"
+  url "https://github.com/liliu-z/stashbase/releases/download/v0.2.0/StashBase-0.2.0-mac-arm64.dmg"
   name "StashBase"
   desc "Local-first knowledge base desktop app."
   homepage "https://github.com/liliu-z/stashbase"
